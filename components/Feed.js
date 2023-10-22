42a4e5931074a0874e4081586f902783ae02cb6e
Renil Soni <srenil063@gmail.com>
2023-10-21 23:46:58 -0400

    added login and lgout functionlity with sidebar and header

---
+++ components/Feed.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Feed = () => {
+  return (
+    <div className='flex-grow'>Feed</div>
+  )
+}
+
+export default Feed
---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "upload.wikimedia.org", 
+      "static.xx.fbcdn.net", 
+      "scontent.fyzd1-3.fna.fbcdn.net",
+      "platform-lookaside.fbsbx.com",
+    ],
+  },
+};
+
+module.exports = nextConfig
